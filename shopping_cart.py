14891962bd8d32b3b5eed58cdd01f0cd8a467b87
cassiehenning <cjh281@georgetown.edu>
2021-02-23 19:44:26 -0500

    Lookup Product Given Identifier

--- shopping_cart.py
+++ shopping_cart.py
@@ -44,8 +44,16 @@ def to_usd(my_price):
 
 selected_id = input("Please input a product identifier:")
 
-print(selected_id)
-print(type(selected_id))
+matching_products = [p for p in products if str(p["id"]) == str(selected_id)]
+
+matching_product = matching_products[0]
+
+print("SELECTED PRODUCT: " + str(matching_product["name"]) + " " + str(matching_product["price"])) 
+
+
+#print(matching_product)
+#print(type(matching_product))
+
 
 
 #A grocery store name of your choice
