4c1a7e7b571466636e8f570339c9930c8663a177
cassiehenning <cjh281@georgetown.edu>
2021-02-23 19:04:46 -0500

    Capture User Input

--- shopping_cart.py
+++ shopping_cart.py
@@ -39,8 +39,13 @@ def to_usd(my_price):
 
 # TODO: write some Python code here to produce the desired output
 
-print(products)
+#print(products)
 
+
+selected_id = input("Please input a product identifier:")
+
+print(selected_id)
+print(type(selected_id))
 
 
 #A grocery store name of your choice
